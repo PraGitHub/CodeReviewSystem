fd2381868ef10d5b4451f192c095c7ffae13d425
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-08-26 09:47:44 +0530

    .

--- HelperFunctions.js
+++ HelperFunctions.js
@@ -5,7 +5,7 @@ var fpGetHttpPort = function GetHttpPort(){
     var strPort = "";
     for(let i=0;i<process.argv.length;i++){
         var strArg = process.argv[i];
-        if(strArg.indexOf('-port:')>-1){
+        if(strArg.indexOf('-port=')>-1){
             strPort = strArg.substr(6);
         }
     }
