efeacab7701a17d0bebdb627783d2f16d07109d6
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-09-02 16:17:36 +0530

    CryptrWrapper_NodeMailer

---
+++ src/CryptrWrapper.js
@@ -0,0 +1,5 @@
+/*
+This should be a wrapper for 'cryptr'.
+Customised according to our requirement
+look for npm encryptr
+*/
---
+++ src/eMailUser.js
@@ -0,0 +1,5 @@
+/*
+This is to implement email sender. 
+A new user should be sent an e-mail while registering in order to check users' authenticity
+look for npm nodemailer
+*/
