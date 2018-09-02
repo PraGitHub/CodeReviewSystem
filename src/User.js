653c4e509c67a1e3095a80a41d2a384a5a181bd7
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2018-09-02 11:49:01 +0530

    UserClass

---
+++ src/User.js
@@ -0,0 +1,10 @@
+/*
+This file is to implement a class which is going to hold all the details of the current user.
+*/
+class user{
+    constructor(jsonAttributes = {}){
+
+    }
+}
+
+module.exports.user = user;
